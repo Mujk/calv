55dc62d0afa5814373aa2339ddb88d6b1e2d5574
Florian Mujkanovic <mujk@proton.me>
2024-08-03 10:23:48 +0200

    Add: add appointment option -a

--- calv.sh
+++ calv.sh
@@ -1,1 +1,12 @@
 #! /bin/sh
+
+set -x
+
+location="$HOME/.appointments"
+
+if [ "$1" = "-a" ]; then
+    title="$2"
+    description="$3"
+    date="$4"
+    echo "$title"'\n'"$description"'\n'"$date"'\n' >> "$location"
+fi
