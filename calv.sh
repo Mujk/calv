d59300254f9dda0987bf9c0cba938c88ed42ebcd
Florian Mujkanovic <mujk@proton.me>
2024-08-04 19:42:13 +0200

    Add: delete option (WIP)
    Reading the correct lines doesn't work properly

--- calv.sh
+++ calv.sh
@@ -4,9 +4,26 @@ set -x
 
 location="$HOME/.appointments"
 
-if [ "$1" = "-a" ]; then
-    title="$2"
-    description="$3"
-    date="$4"
+option="$1"
+title="$2"
+description="$3"
+date="$4"
+if [ "$option" = "-a" ]; then
     echo "$title"'\n'"$description"'\n'"$date"'\n' >> "$location"
+elif [ "$option" = "-d" ]; then
+    title="$2"
+    echo "grep-result: ""$(grep -n "$title" "$location" | cut -d : -f 1)|tr '\n' ' '"
+    set -- "$(grep -n "$title" "$location" | cut -d : -f 1)|tr '\n' ' '"
+    echo "found: ""$*"
+    if [ -z "$3" ]; then
+        for line in "$lines_with_title"
+        do
+            last_line="$(($line+2))"
+            sed -i ""$line", "$last_line"d" "$location"
+       	done
+    else
+        date="$4"
+        lines_with_date=grep -n "$date" "$location" | cut -d : -f 1 | head -1
+#       sed -i '"$lines_with_title", "$lines_with_title"d' "$appointments"
+    fi
 fi
