e4cfa9ee7d16a1f5e27846506d4a8c898cc1965f
Florian Mujkanovic <mujk@proton.me>
2024-08-06 22:49:38 +0200

    refactor: give line variables better names

--- calv.sh
+++ calv.sh
@@ -12,10 +12,10 @@ if [ "$option" = "-a" ]; then
     echo "$title"'\n'"$description"'\n'"$date"'\n' >> "$location"
 elif [ "$option" = "-d" ]; then
     title="$2"
-    lines="$(grep -n "$title" "$location" | cut -d : -f 1)"
+    title_lines="$(grep -n "$title" "$location" | cut -d : -f 1)"
     deleted_lines=0
     if [ -z "$date" ]; then
-        echo "$lines" | while read line; do
+        echo "$title_lines" | while read line; do
           line="$(($line-$deleted_lines))"
           last_line="$(($line+4))"
           sed -i ""$line", "$last_line"d" "$location"
@@ -23,7 +23,7 @@ elif [ "$option" = "-d" ]; then
         done
     else
         date="$4"
-        lines_with_date=grep -n "$date" "$location" | cut -d : -f 1 | head -1
+        date_lines=grep -n "$date" "$location" | cut -d : -f 1 | head -1
 #       sed -i '"$lines_with_title", "$lines_with_title"d' "$appointments"
     fi
 fi
